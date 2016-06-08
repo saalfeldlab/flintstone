f60fc67c81423a86e9ef27f4d03d0f12d8b97574
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-06-08 11:11:40 -0400

    Added runtime option and fixed: MASTER_GREP Warning: no access to tty (Bad file descriptor) bug using \#$ -S /bin/bash at first line of script Output looks better now

--- flintstone.sh
+++ flintstone.sh
@@ -3,7 +3,7 @@
 SPARK_DEPLOY_CMD="/usr/local/python-2.7.6/bin/python /usr/local/spark-versions/bin/spark-deploy.py"
 
 USAGE="usage:
-[TERMINATE=1] $0 <MASTER_JOB_ID|N_NODES> <JAR> <CLASS> <ARGV>
+[TERMINATE=1] [RUNTIME=<hh:mm:ss>] $0 <MASTER_JOB_ID|N_NODES> <JAR> <CLASS> <ARGV>
 
 If job with \${MASTER_JOB_ID} does not exist, value will be interpreted as number of 
 nodes (N_NODES), and a new Spark master will be started with N_NODES workers using
@@ -14,6 +14,7 @@ If master exists but no workers are present, this script will just exit with a n
 value."
 
 FAILURE_CODE=1
+RUNTIME="${RUNTIME:-default}"
 
 if [ "$#" -lt "3" ]; then
     echo -e "Not enough arguments!" 1>&2
@@ -29,8 +30,7 @@ JAR=`readlink -f $1`; shift
 CLASS=$1;             shift
 
 ARGV="$@"
-
-MASTER_GREP=`qstat | grep -E  "${MASTER_JOB_ID} [0-9.]+ master"`
+MASTER_GREP=`qstat | grep -E  "^ +${MASTER_JOB_ID} [0-9.]+ master"`
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" -ne "0" ]; then
@@ -52,16 +52,13 @@ if [ "$EXIT_CODE" -ne "0" ]; then
     SUBMISSION=`qsub -jc sparkflex.default`
     N_NODES=${MASTER_JOB_ID}
     MASTER_JOB_ID=`echo $SUBMISSION | sed -r -e 's/Your job ([0-9]+) .*/\\1/'`
-    MASTER_GREP=`qstat | grep -E  "${MASTER_JOB_ID} [0-9.]+ master"`
-    ${SPARK_DEPLOY_CMD} -j $MASTER_JOB_ID -w $N_NODES
+    MASTER_GREP=`qstat | grep -E  "^ +${MASTER_JOB_ID} [0-9.]+ master"`
+    # MASTER_GREP=`qstat | grep -E  "${MASTER_JOB_ID} [0-9.]+ master"`
+    ${SPARK_DEPLOY_CMD} -j $MASTER_JOB_ID -w ${N_NODES} -t ${RUNTIME}
 fi
 
-# echo ${MASTER_JOB_ID}
-# qstat |  grep "W${MASTER_JOB_ID}" | wc -l
-# exit 34
 
 N_NODES=`qstat | grep "W${MASTER_JOB_ID}" | wc -l`
-# echo $N_NODES
 
 if [ "$N_NODES" -lt "1" ]; then
     echo -e "No workers present for master ${MASTER_JOB_ID}!"
@@ -74,36 +71,61 @@ fi
 while [ -n "$(echo $MASTER_GREP | grep qw)" ] ; do
     echo "Master node not ready yet (qw) - try again in five seconds..."
     sleep 5s
-    MASTER_GREP=`qstat | grep -E  "${MASTER_JOB_ID} [0-9.]+ master"`
+    MASTER_GREP=`qstat | grep -E  "^ +${MASTER_JOB_ID} [0-9.]+ master"`
+    # MASTER_GREP=`qstat | grep -E  "${MASTER_JOB_ID} [0-9.]+ master"`
 done
 HOST=`echo $MASTER_GREP | sed -r -e 's/.* hadoop[A-Za-z0-9.]+@([A-Za-z0-9.]+) .*/\\1/'`
 N_CORES_PER_MACHINE=16
 
-echo $HOST
+TMP_FILE=`mktemp`
+# need this first line in tmp file
+# http://llama.mshri.on.ca/faq-llama.html#tty
+# or run qsub -S /bin/bash 
+echo "#$ -S /bin/bash" >> $TMP_FILE
+chmod +x $TMP_FILE
+echo >> $TMP_FILE
 
-export SPARK_HOME=${SPARK_HOME:-/usr/local/spark-current}
-export PATH=$SPARK_HOME:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
-export PARALLELISM=$(($N_NODES * $N_CORES_PER_MACHINE * 3))
+export SPARK_HOME="${SPARK_HOME:-/usr/local/spark-current}"
+export PATH="$SPARK_HOME:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH"
+export PARALLELISM="$(($N_NODES * $N_CORES_PER_MACHINE * 3))"
 export MASTER="spark://${HOST}:7077"
 
-echo SPARK_HOME $SPARK_HOME
-echo PATH $PATH
-echo PARALLELISM $PARALLELISM
-echo HOST $HOST
-echo MASTER $MASTER
+echo "export SPARK_HOME=${SPARK_HOME}" >> $TMP_FILE
+echo "export PATH=$PATH" >> $TMP_FILE
+echo "export PARALLELISM=$PARALLELISM" >> $TMP_FILE
+echo "export MASTER=$MASTER" >> $TMP_FILE
+
+echo -e
+echo -e Environment:
+echo -e "SPARK_HOME       $SPARK_HOME"
+echo -e "PATH             ${PATH//:/\n                 }"
+echo -e "PARALLELISM      $PARALLELISM"
+echo -e "HOST             $HOST"
+echo -e "MASTER           $MASTER"
+echo -e "JOB_FILE         $TMP_FILE"
+echo -e "JOB_NAME         $CLASS"
 
 mkdir -p ~/.sparklogs
 
-TIME_CMD="time $SPARK_HOME/bin/spark-submit"
-$TIME_CMD --verbose \
+echo TIME_CMD="\"time \$SPARK_HOME/bin/spark-submit\"" >> $TMP_FILE
+echo \$TIME_CMD --verbose \
           --conf spark.default.parallelism=$PARALLELISM \
           --class $CLASS \
           $JAR \
-          $ARGV
+          $ARGV >> $TMP_FILE
 
-if [ -n "$TERMINATE" ]; then
-    ${SPARK_DEPLOY_CMD} -s -j ${MASTER_JOB_ID} -f
+if [ -n "${TERMINATE}" ]; then
+    echo "${SPARK_DEPLOY_CMD} -s -j ${MASTER_JOB_ID} -f"  >> $TMP_FILE
 fi
+
+
+
+JOB_MESSAGE=`qsub -pe batch 16 -N "$CLASS" -j y -o ~/.sparklogs/ $TMP_FILE`
+JOB_ID=`echo ${JOB_MESSAGE} | sed -r 's/Your job ([0-9]+) .*/\1/'`
+echo -e "JOB_ID           $JOB_ID"
+
+echo
+echo -e $JOB_MESSAGE
 
 
 
